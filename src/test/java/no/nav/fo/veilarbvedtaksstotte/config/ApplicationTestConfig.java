4a69aaf4be862250eefcae849f061beda4027b76
matiasvinjevoll <matias.vinjevoll@nav.no>
2019-04-29 09:00:32 +0200

    KAIZEN-0 migrer in memory database i test config

--- src/test/java/no/nav/fo/veilarbvedtaksstotte/config/ApplicationTestConfig.java
+++ src/test/java/no/nav/fo/veilarbvedtaksstotte/config/ApplicationTestConfig.java
@@ -2,9 +2,11 @@ package no.nav.fo.veilarbvedtaksstotte.config;
 
 import no.nav.apiapp.security.PepClient;
 import no.nav.dialogarena.aktor.AktorService;
+import no.nav.fo.veilarbvedtaksstotte.db.DatabaseTestContext;
 import no.nav.fo.veilarbvedtaksstotte.mock.AktorServiceMock;
 import no.nav.fo.veilarbvedtaksstotte.mock.Mock;
 import no.nav.fo.veilarbvedtaksstotte.mock.PepClientMock;
+import no.nav.fo.veilarbvedtaksstotte.utils.DbUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +20,9 @@ public class ApplicationTestConfig extends ApplicationConfig {
 
     @Override
     public void startup(ServletContext servletContext) {
-        // NO-OP
+        if (DatabaseTestContext.isInMemoryDatabase()) {
+            DbUtils.migrate(jdbcTemplate);
+        }
     }
 
     @Bean
