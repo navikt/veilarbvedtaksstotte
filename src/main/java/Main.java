9054de68ec8e4e3f16b08de10e2763979cbeb3f6
Alexander Gård <alex.gaard@hotmail.com>
2019-11-08 10:43:23 +0100

    KAIZEN-0 Migrering til naiserator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import no.nav.apiapp.ApiApp;
+import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.common.utils.NaisUtils;
 import no.nav.fo.veilarbvedtaksstotte.config.ApplicationConfig;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 import no.nav.sbl.util.EnvironmentUtils;
 
 import static java.lang.System.setProperty;
+import static no.nav.common.utils.NaisUtils.getCredentials;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbvedtaksstotte.client.CVClient.PAM_CV_API;
 import static no.nav.fo.veilarbvedtaksstotte.client.CVClient.CV_API_PROPERTY_NAME;
@@ -24,12 +29,32 @@ import static no.nav.fo.veilarbvedtaksstotte.client.RegistreringClient.VEILARBRE
 import static no.nav.fo.veilarbvedtaksstotte.client.RegistreringClient.REGISTRERING_API_PROPERTY_NAME;
 import static no.nav.fo.veilarbvedtaksstotte.client.SAFClient.SAF;
 import static no.nav.fo.veilarbvedtaksstotte.client.SAFClient.SAF_API_PROPERTY_NAME;
+import static no.nav.fo.veilarbvedtaksstotte.config.ApplicationConfig.SECURITYTOKENSERVICE_URL;
+import static no.nav.fo.veilarbvedtaksstotte.config.DatabaseConfig.VEILARBVEDTAKSSTOTTE_DB_PASSWORD;
+import static no.nav.fo.veilarbvedtaksstotte.config.DatabaseConfig.VEILARBVEDTAKSSTOTTE_DB_USERNAME;
 import static no.nav.fo.veilarbvedtaksstotte.utils.UrlUtils.lagClusterUrl;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
 
     public static void main(String... args) {
+        readFromConfigMap();
+
+        NaisUtils.Credentials serviceUser = getCredentials("service_user");
+
+        //STS
+        setProperty(StsSecurityConstants.STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL));
+        setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //ABAC
+        System.setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //OIDC
+        System.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
         setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty("AKTOER_V2_ENDPOINTURL"));
         setProperty(DOKUMENT_API_PROPERTY_NAME, lagClusterUrl(VEILARBDOKUMENT));
         setProperty(PERSON_API_PROPERTY_NAME, lagClusterUrl(VEILARBPERSON));
@@ -49,7 +74,30 @@ public class Main {
             setProperty(CV_API_PROPERTY_NAME, lagClusterUrl(PAM_CV_API));
         }
 
+        NaisUtils.Credentials oracleCreds = getCredentials("oracle_creds");
+        System.setProperty(VEILARBVEDTAKSSTOTTE_DB_USERNAME, oracleCreds.username);
+        System.setProperty(VEILARBVEDTAKSSTOTTE_DB_PASSWORD, oracleCreds.password);
+
         ApiApp.startApiApp(ApplicationConfig.class, args);
+    }
+
+    private static void readFromConfigMap() {
+        NaisUtils.addConfigMapToEnv("pto-config",
+                "KAFKA_BROKERS_URL",
+                "AKTOER_V2_SECURITYTOKEN",
+                "AKTOER_V2_ENDPOINTURL",
+                "AKTOER_V2_WSDLURL",
+                "ABAC_PDP_ENDPOINT_URL",
+                "ABAC_PDP_ENDPOINT_DESCRIPTION",
+                "ISSO_HOST_URL",
+                "ISSO_JWKS_URL",
+                "ISSO_ISSUER_URL",
+                "ISSO_ISALIVE_URL",
+                "SECURITYTOKENSERVICE_URL",
+                "UNLEASH_API_URL",
+                "VEILARBLOGIN_REDIRECT_URL_DESCRIPTION",
+                "OIDC_REDIRECT_URL"
+        );
     }
 
 }
