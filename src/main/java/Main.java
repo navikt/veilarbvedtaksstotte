e6554c82746f849cc6d1828578bc2a4c3bfe6b8d
Alexander Gård <alex.gaard@hotmail.com>
2020-01-15 15:42:28 +0100

    KAIZEN-0 Gå mot PAM i Q0 og ikke Q6 når vi er i Q1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -66,10 +66,10 @@ public class Main {
         setProperty(SAF_API_PROPERTY_NAME, lagClusterUrl(SAF,false));
         setProperty(VEILARBOPPFOLGING_API_PROPERTY_NAME, lagClusterUrl(VEILARBOPPFOLGING));
 
-        // PAM CV finnes ikke i Q1, gå mot Q6 istedenfor
+        // PAM CV finnes ikke i Q1, gå mot Q0 istedenfor
         // Oppgave i Q1 ligger på default namespace
         if (EnvironmentUtils.requireEnvironmentName().equalsIgnoreCase("q1")) {
-            String pamCVUrl = lagClusterUrl(PAM_CV_API).replace("q1", "q6");
+            String pamCVUrl = lagClusterUrl(PAM_CV_API).replace("q1", "q0");
             setProperty(CV_API_PROPERTY_NAME, pamCVUrl);
             setProperty(OPPGAVE_API_PROPERTY_NAME, "https://oppgave.nais.preprod.local");
         } else {
@@ -87,14 +87,12 @@ public class Main {
                 "AKTOER_V2_ENDPOINTURL",
                 "AKTOER_V2_WSDLURL",
                 "ABAC_PDP_ENDPOINT_URL",
-                "ABAC_PDP_ENDPOINT_DESCRIPTION",
                 "ISSO_HOST_URL",
                 "ISSO_JWKS_URL",
                 "ISSO_ISSUER_URL",
                 "ISSO_ISALIVE_URL",
                 "SECURITYTOKENSERVICE_URL",
                 "UNLEASH_API_URL",
-                "VEILARBLOGIN_REDIRECT_URL_DESCRIPTION",
                 "OIDC_REDIRECT_URL"
         );
     }
