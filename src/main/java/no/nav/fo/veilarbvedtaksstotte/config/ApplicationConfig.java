3e9d2efa73284ce32a22d733674b027d6862e13d
Alexander Gård <alex.gaard@hotmail.com>
2020-01-24 09:48:22 +0100

    FO-2687 La til støtte for å validere Azure AD tokens

--- src/main/java/no/nav/fo/veilarbvedtaksstotte/config/ApplicationConfig.java
+++ src/main/java/no/nav/fo/veilarbvedtaksstotte/config/ApplicationConfig.java
@@ -3,6 +3,8 @@ package no.nav.fo.veilarbvedtaksstotte.config;
 import no.nav.apiapp.ApiApplication;
 import no.nav.apiapp.ServletUtil;
 import no.nav.apiapp.config.ApiAppConfigurator;
+import no.nav.brukerdialog.security.domain.IdentType;
+import no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig;
 import no.nav.dialogarena.aktor.AktorConfig;
 import no.nav.fo.veilarbvedtaksstotte.utils.DbRole;
 import no.nav.fo.veilarbvedtaksstotte.utils.DbUtils;
@@ -15,8 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.ServletContext;
 
+import static no.nav.brukerdialog.security.Constants.AZUREADB2C_OIDC_COOKIE_NAME_FSS;
 import static no.nav.fo.veilarbvedtaksstotte.config.DatabaseConfig.VEILARBVEDTAKSSTOTTE_DB_URL_PROPERTY;
-import static no.nav.fo.veilarbvedtaksstotte.utils.DbUtils.toDbRoleStr;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 
@@ -47,10 +49,20 @@ public class ApplicationConfig implements ApiApplication {
 
     @Override
     public void configure(ApiAppConfigurator apiAppConfigurator) {
+        String discoveryUrl = getRequiredProperty("AAD_DISCOVERY_URL");
+        String clientId = getRequiredProperty("VEILARBLOGIN_AAD_CLIENT_ID");
+
+        AzureADB2CConfig config = AzureADB2CConfig.builder()
+                .discoveryUrl(discoveryUrl)
+                .expectedAudience(clientId)
+                .identType(IdentType.InternBruker)
+                .tokenName(AZUREADB2C_OIDC_COOKIE_NAME_FSS)
+                .build();
+
         apiAppConfigurator
                 .issoLogin()
+                .validateAzureAdInternalUsersTokens(config)
                 .sts();
-
     }
 
     @Transactional
