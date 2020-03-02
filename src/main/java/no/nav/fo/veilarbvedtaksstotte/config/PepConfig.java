048791d8abf128352d0f37b1d673a9056c96fc69
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-03-02 14:00:40 +0100

    KAIZEN-0 ta bort veilarbabac url

--- src/main/java/no/nav/fo/veilarbvedtaksstotte/config/PepConfig.java
+++ src/main/java/no/nav/fo/veilarbvedtaksstotte/config/PepConfig.java
@@ -12,9 +12,6 @@ import org.springframework.context.annotation.Import;
 @Import({AbacContext.class})
 public class PepConfig {
 
-    public static final String VEILARBABAC_API_URL_PROPERTY = "VEILARBABAC_API_URL";
-    public static final String VEILARBABAC = "veilarbabac";
-
     @Bean
     public PepClient pepClient(Pep pep) {
         return new PepClient(pep, "veilarb", ResourceType.VeilArbPerson);
