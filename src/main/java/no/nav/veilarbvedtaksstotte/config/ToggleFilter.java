9f98465595de012e384fc4ce01179ed75d5e315a
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-05-06 16:06:49 +0200

    legg til feature toggle for pilot

--- src/main/java/no/nav/veilarbvedtaksstotte/config/ToggleFilter.java
+++ src/main/java/no/nav/veilarbvedtaksstotte/config/ToggleFilter.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class ToggleFilter implements Filter {
 
     private static final String VEILARBVEDTAKSSTOTTE_ENABLED_TOGGLE = "veilarbvedtaksstotte.enabled";
+    private static final String PTO_VEDTAKSSTOTTE_PILOT_TOGGLE = "pto.vedtaksstotte.pilot";
 
     private UnleashService unleashService;
 
@@ -23,7 +24,8 @@ public class ToggleFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         if (httpServletRequest.getServletPath().startsWith("/internal") ||
-                unleashService.isEnabled(VEILARBVEDTAKSSTOTTE_ENABLED_TOGGLE)) {
+                unleashService.isEnabled(VEILARBVEDTAKSSTOTTE_ENABLED_TOGGLE) ||
+                unleashService.isEnabled(PTO_VEDTAKSSTOTTE_PILOT_TOGGLE)) {
             chain.doFilter(request, response);
         } else {
             throw new IllegalStateException("ikke tilgjengelig");
