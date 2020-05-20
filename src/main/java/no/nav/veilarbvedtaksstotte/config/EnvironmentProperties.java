b67d12ef3c2547a52c4c8ec77f2be7830ee50427
Alexander Gård <alex.gaard@hotmail.com>
2020-05-20 09:31:06 +0200

    KAIZEN-0 Fjernet dobbel bean for EnvironmentProperties og fikset pom for lokal kjøring

--- src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.java
+++ src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.java
@@ -3,11 +3,9 @@ package no.nav.veilarbvedtaksstotte.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Configuration
 @ConfigurationProperties(prefix = "app.env")
 public class EnvironmentProperties {
 
