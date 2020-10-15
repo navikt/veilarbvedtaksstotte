526ab1331802518215c4a900d203bd4fdb5d4a25
Alexander Gård <alex.gaard@hotmail.com>
2020-10-15 13:24:34 +0200

    Bruk ingresser istedenfor service url for å migrere til team namespace

--- src/main/java/no/nav/veilarbvedtaksstotte/config/ClientConfig.java
+++ src/main/java/no/nav/veilarbvedtaksstotte/config/ClientConfig.java
@@ -1,54 +1,62 @@
 package no.nav.veilarbvedtaksstotte.config;
 
+import no.nav.common.utils.EnvironmentUtils;
 import no.nav.veilarbvedtaksstotte.client.*;
 import no.nav.veilarbvedtaksstotte.client.api.*;
 import no.nav.veilarbvedtaksstotte.service.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static no.nav.common.utils.UrlUtils.createServiceUrl;
+import static no.nav.common.utils.UrlUtils.createNaisAdeoIngressUrl;
+import static no.nav.common.utils.UrlUtils.createNaisPreprodIngressUrl;
 
 @Configuration
 public class ClientConfig {
 
     @Bean
     public ArenaClient arenaClient() {
-        return new ArenaClientImpl(createServiceUrl("veilarbarena", true));
+        return new ArenaClientImpl(naisPreprodOrNaisAdeoIngress("veilarbarena",  true));
     }
 
     @Bean
     public DokumentClient dokumentClient() {
-        return new DokumentClientImpl(createServiceUrl("veilarbdokument", true));
+        return new DokumentClientImpl(naisPreprodOrNaisAdeoIngress("veilarbdokument", true));
     }
 
     @Bean
     public EgenvurderingClient egenvurderingClient() {
-        return new EgenvurderingClientImpl(createServiceUrl("veilarbvedtakinfo", true));
+        return new EgenvurderingClientImpl(naisPreprodOrNaisAdeoIngress("veilarbvedtakinfo", true));
     }
 
     @Bean
     public OppfolgingClient oppfolgingClient() {
-        return new OppfolgingClientImpl(createServiceUrl("veilarboppfolging", true));
+        return new OppfolgingClientImpl(naisPreprodOrNaisAdeoIngress("veilarboppfolging", true));
     }
 
     @Bean
     public VeilarbpersonClient personClient(AuthService authService) {
-        return new VeilarbpersonClientImpl(createServiceUrl("veilarbperson", true), authService::getInnloggetBrukerToken);
+        return new VeilarbpersonClientImpl(naisPreprodOrNaisAdeoIngress("veilarbperson", true), authService::getInnloggetBrukerToken);
     }
 
     @Bean
     public RegistreringClient registreringClient() {
-        return new RegistreringClientImpl(createServiceUrl("veilarbregistrering", true));
+        return new RegistreringClientImpl(naisPreprodOrNaisAdeoIngress("veilarbregistrering", true));
     }
 
     @Bean
     public SafClient safClient() {
-        return new SafClientImpl(createServiceUrl("saf", false));
+        return new SafClientImpl(naisPreprodOrNaisAdeoIngress("saf", false));
     }
 
     @Bean
     public VeiledereOgEnhetClient veilederOgEnhetClient() {
-        return new VeiledereOgEnhetClientImpl(createServiceUrl("veilarbveileder", true));
+        return new VeiledereOgEnhetClientImpl(naisPreprodOrNaisAdeoIngress("veilarbveileder", true));
+    }
+
+    private static String naisPreprodOrNaisAdeoIngress(String appName, boolean withAppContextPath) {
+        return  EnvironmentUtils.isDevelopment().orElse(false)
+                ? createNaisPreprodIngressUrl(appName, "q1", withAppContextPath)
+                : createNaisAdeoIngressUrl(appName, withAppContextPath);
     }
 
 }
