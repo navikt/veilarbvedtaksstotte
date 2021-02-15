166dc785e530549629986c83b0b87bc39b76b619
jeeveshmishra <jeevesh.mishra@nav.no>
2021-02-15 10:33:07 +0100

    VIBE-TC-59 Ryddet opp tests og tests funker nå uten SingletonPostgresContainer.

--- src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
+++ src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
@@ -5,39 +5,43 @@ import static org.junit.Assert.assertNotNull;
 
 import no.nav.veilarbvedtaksstotte.client.dokument.DokumentSendtDTO;
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak;
-import no.nav.veilarbvedtaksstotte.utils.SingletonPostgresContainer;
-import no.nav.veilarbvedtaksstotte.utils.TestData;
+import no.nav.veilarbvedtaksstotte.utils.DbTestUtils;
+import no.nav.veilarbvedtaksstotte.utils.PostgresContainer;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.List;
 
 public class FlywayDbMigrationTest {
-    private static JdbcTemplate db;
-    private static TransactionTemplate transactor;
-    private static KilderRepository kilderRepository;
     private static VedtaksstotteRepository vedtaksstotteRepository;
+    private JdbcTemplate db;
+    private TransactionTemplate transactor;
+    private PostgresContainer postgresContainer;
+
+    @Before
+    public void steUp() {
+        postgresContainer = new PostgresContainer();
+    }
+
 
     @Test(expected = Exception.class)
-    public void testBeforeVersion23() {
-        final String versionPrefix = "22";
-        db = SingletonPostgresContainer.init(versionPrefix).getDb();
-        transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
-        kilderRepository = new KilderRepository(db);
+    public void testVersion_before_column_changes_for_v23() {
+        final String versjonPrefix = "22";
+        prepareDatabasen(versjonPrefix);
 
         vedtaksstotteRepository = new VedtaksstotteRepository(db, transactor);
         vedtaksstotteRepository.opprettUtkast(TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID);
         vedtaksstotteRepository.hentUtkast(TEST_AKTOR_ID);
+
     }
 
     @Test
-    public void testVersion23() {
-
-        db = SingletonPostgresContainer.init().getDb();
-        transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
-        kilderRepository = new KilderRepository(db);
+    public void testVersion_after_column_changes_for_v23() {
+        final String versionPrefix = "23";
+        prepareDatabasen(versionPrefix);
 
         DokumentSendtDTO dokumentSendtDTO = new DokumentSendtDTO(TEST_JOURNALPOST_ID, TEST_DOKUMENT_ID);
 
@@ -51,5 +55,22 @@ public class FlywayDbMigrationTest {
         assertNotNull("Vedtak er null", vedtak);
         assertNotNull(vedtak.getUtkastSistOppdatert());
         assertNotNull("Vedtak fattet dato er null", vedtak.getVedtakFattet());
+    }
+
+    @AfterEach
+    public void setupShutdownHook() {
+        DbTestUtils.cleanupDb(db);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (postgresContainer != null) {
+                postgresContainer.stopContainer();
+            }
+        }));
+    }
+
+    private void prepareDatabasen(String versjonPrefix) {
+        db = postgresContainer.getDb();
+        transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
+
+        DbTestUtils.testMigrate(postgresContainer.getDataSource(), versjonPrefix);
     }
 }
