09f7a2cbb392b93b7cab964612ecdf72f953cbc4
jeeveshmishra <jeevesh.mishra@nav.no>
2021-02-22 17:33:38 +0100

    VIBE-TC-59 Endret test å være mer verdifult.

--- src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
+++ src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
@@ -1,10 +1,13 @@
 package no.nav.veilarbvedtaksstotte.repository;
 
+import static java.lang.String.format;
 import static no.nav.veilarbvedtaksstotte.utils.TestData.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import no.nav.veilarbvedtaksstotte.client.dokument.DokumentSendtDTO;
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak;
+import no.nav.veilarbvedtaksstotte.domain.vedtak.VedtakStatus;
 import no.nav.veilarbvedtaksstotte.utils.DbTestUtils;
 import no.nav.veilarbvedtaksstotte.utils.PostgresContainer;
 import org.junit.Before;
@@ -14,9 +17,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 
 public class FlywayDbMigrationTest {
-    private static VedtaksstotteRepository vedtaksstotteRepository;
     private JdbcTemplate db;
     private TransactionTemplate transactor;
     private PostgresContainer postgresContainer;
@@ -28,33 +33,37 @@ public class FlywayDbMigrationTest {
 
 
     @Test(expected = Exception.class)
-    public void testVersion_before_column_changes_for_v23() {
+    public void testVersion_for_og_etter_colon_endringene_til_v23() {
         final String versjonPrefix = "22";
+        final String versjonEtterVedtakOppdatert = "23";
         prepareDatabasen(versjonPrefix);
 
-        vedtaksstotteRepository = new VedtaksstotteRepository(db, transactor);
-        vedtaksstotteRepository.opprettUtkast(TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID);
-        vedtaksstotteRepository.hentUtkast(TEST_AKTOR_ID);
+        LocalDateTime todaysDate = LocalDateTime.now();
 
-    }
+        String sql = format(
+                "INSERT INTO %s(%s, %s, %s, %s, %s) values(?, ?, ?, ?, CURRENT_TIMESTAMP)",
+                "VEDTAK", TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID, VedtakStatus.UTKAST, todaysDate
+        );
+        db.execute(sql);
 
-    @Test
-    public void testVersion_after_column_changes_for_v23() {
-        final String versionPrefix = "23";
-        prepareDatabasen(versionPrefix);
+        prepareDatabasen(versjonEtterVedtakOppdatert);
+
+
+        VedtaksstotteRepository vedtaksstotteRepository = new VedtaksstotteRepository(db, transactor);
+        Vedtak utkastVedtak = vedtaksstotteRepository.hentUtkast(TEST_AKTOR_ID);
+
+        assertNotNull("Vedtak er null", utkastVedtak);
+        assertEquals(todaysDate, utkastVedtak.getUtkastSistOppdatert());
+        assertNotNull("Vedtak fattet dato er null", utkastVedtak.getVedtakFattet());
 
         DokumentSendtDTO dokumentSendtDTO = new DokumentSendtDTO(TEST_JOURNALPOST_ID, TEST_DOKUMENT_ID);
+        vedtaksstotteRepository.ferdigstillVedtak(utkastVedtak.getId(), dokumentSendtDTO);
+        Vedtak vedtakFattet = vedtaksstotteRepository.hentVedtak(utkastVedtak.getId());
 
-        vedtaksstotteRepository = new VedtaksstotteRepository(db, transactor);
-        vedtaksstotteRepository.opprettUtkast(TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID);
-        Vedtak vedtak = vedtaksstotteRepository.hentUtkast(TEST_AKTOR_ID);
-        vedtaksstotteRepository.ferdigstillVedtak(vedtak.getId(), dokumentSendtDTO);
-        vedtak = vedtaksstotteRepository.hentVedtak(vedtak.getId());
+        assertNotNull("Vedtak er null", vedtakFattet);
+        assertEquals(todaysDate, vedtakFattet.getUtkastSistOppdatert());
+        assertNotNull("Vedtak fattet dato er null", utkastVedtak.getVedtakFattet());
 
-
-        assertNotNull("Vedtak er null", vedtak);
-        assertNotNull(vedtak.getUtkastSistOppdatert());
-        assertNotNull("Vedtak fattet dato er null", vedtak.getVedtakFattet());
     }
 
     @AfterEach
@@ -69,7 +78,11 @@ public class FlywayDbMigrationTest {
 
     private void prepareDatabasen(String versjonPrefix) {
         db = postgresContainer.getDb();
-        transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
+
+        boolean isDbNull = db != null && db.getDataSource() != null;
+        if (isDbNull) {
+            transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
+        }
 
         DbTestUtils.testMigrate(postgresContainer.getDataSource(), versjonPrefix);
     }
