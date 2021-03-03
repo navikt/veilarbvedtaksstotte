e1d417e647fa187233c223f11882475d8d8d4168
jeeveshmishra <jeevesh.mishra@nav.no>
2021-03-03 23:14:17 +0100

    VIBE-TC-59 Forbedret tests på 2 forskjellige nivå - (1) Database Nivå for å teste flyway migrering. (2) Mapping nivå for å teste ny kolon er på plass.

--- src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
+++ src/test/java/no/nav/veilarbvedtaksstotte/repository/FlywayDbMigrationTest.java
@@ -1,12 +1,5 @@
 package no.nav.veilarbvedtaksstotte.repository;
 
-import static java.lang.String.format;
-import static no.nav.veilarbvedtaksstotte.utils.TestData.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import no.nav.veilarbvedtaksstotte.client.dokument.DokumentSendtDTO;
-import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak;
 import no.nav.veilarbvedtaksstotte.domain.vedtak.VedtakStatus;
 import no.nav.veilarbvedtaksstotte.utils.DbTestUtils;
 import no.nav.veilarbvedtaksstotte.utils.PostgresContainer;
@@ -14,56 +7,69 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.Optional;
+
+import static no.nav.veilarbvedtaksstotte.domain.vedtak.VedtakStatus.SENDT;
+import static no.nav.veilarbvedtaksstotte.domain.vedtak.VedtakStatus.UTKAST;
+import static no.nav.veilarbvedtaksstotte.utils.DbUtils.queryForObjectOrNull;
+import static no.nav.veilarbvedtaksstotte.utils.TestData.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 
 public class FlywayDbMigrationTest {
     private JdbcTemplate db;
-    private TransactionTemplate transactor;
     private PostgresContainer postgresContainer;
 
     @Before
     public void steUp() {
         postgresContainer = new PostgresContainer();
+        db = postgresContainer.getDb();
     }
 
-
-    @Test(expected = Exception.class)
+    @Test
     public void testVersion_for_og_etter_colon_endringene_til_v23() {
         final String versjonPrefix = "22";
         final String versjonEtterVedtakOppdatert = "23";
         prepareDatabasen(versjonPrefix);
-
-        LocalDateTime todaysDate = LocalDateTime.now();
-
-        String sql = format(
-                "INSERT INTO %s(%s, %s, %s, %s, %s) values(?, ?, ?, ?, CURRENT_TIMESTAMP)",
-                "VEDTAK", TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID, VedtakStatus.UTKAST, todaysDate
-        );
-        db.execute(sql);
-
+        LocalDateTime utkastDate = LocalDateTime.now();
+        LocalDateTime fattetDate = LocalDateTime.now().minusDays(1);
+        String sql =
+                "INSERT INTO VEDTAK(AKTOR_ID, VEILEDER_IDENT, OPPFOLGINGSENHET_ID, STATUS, SIST_OPPDATERT)"
+                        + " values(?, ?, ?, ?, ?)";
+        db.update(sql, TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID, SENDT.name(), fattetDate);
+        db.update(sql, TEST_AKTOR_ID, TEST_VEILEDER_IDENT, TEST_OPPFOLGINGSENHET_ID, UTKAST.name(), utkastDate);
+        LocalDateTime sistOppdatertFattet = hentDateKolonne("SIST_OPPDATERT", SENDT);
+        LocalDateTime sistOppdatertUtkast = hentDateKolonne("SIST_OPPDATERT", UTKAST);
+        assertEquals(fattetDate, sistOppdatertFattet);
+        assertEquals(utkastDate, sistOppdatertUtkast);
         prepareDatabasen(versjonEtterVedtakOppdatert);
+        LocalDateTime utkastSistOppdatertFattet = hentDateKolonne("UTKAST_SIST_OPPDATERT", SENDT);
+        LocalDateTime vedtakFattetFattet = hentDateKolonne("VEDTAK_FATTET", SENDT);
+        LocalDateTime utkastSistOppdatertUtkast = hentDateKolonne("UTKAST_SIST_OPPDATERT", UTKAST);
+        LocalDateTime vedtakFattetUtkast = hentDateKolonne("VEDTAK_FATTET", UTKAST);
+        assertEquals(sistOppdatertFattet, utkastSistOppdatertFattet);
+        assertEquals(sistOppdatertFattet, vedtakFattetFattet);
+        assertEquals(sistOppdatertUtkast, utkastSistOppdatertUtkast);
+        assertNull("Utkast skal ikke ha verdi for VEDTAK_FATTET", vedtakFattetUtkast);
+    }
 
+    private LocalDateTime hentDateKolonne(String dateKolonne, VedtakStatus vedtakStatus) {
+        return queryForObjectOrNull(() ->
+                db.queryForObject(
+                        "SELECT * FROM VEDTAK WHERE STATUS = ?",
+                        (rs, rowNum) ->
+                                Optional.ofNullable(rs.getTimestamp(dateKolonne))
+                                        .map(Timestamp::toLocalDateTime)
+                                        .orElse(null),
+                        vedtakStatus.name()));
+    }
 
-        VedtaksstotteRepository vedtaksstotteRepository = new VedtaksstotteRepository(db, transactor);
-        Vedtak utkastVedtak = vedtaksstotteRepository.hentUtkast(TEST_AKTOR_ID);
-
-        assertNotNull("Vedtak er null", utkastVedtak);
-        assertEquals(todaysDate, utkastVedtak.getUtkastSistOppdatert());
-        assertNotNull("Vedtak fattet dato er null", utkastVedtak.getVedtakFattet());
-
-        DokumentSendtDTO dokumentSendtDTO = new DokumentSendtDTO(TEST_JOURNALPOST_ID, TEST_DOKUMENT_ID);
-        vedtaksstotteRepository.ferdigstillVedtak(utkastVedtak.getId(), dokumentSendtDTO);
-        Vedtak vedtakFattet = vedtaksstotteRepository.hentVedtak(utkastVedtak.getId());
-
-        assertNotNull("Vedtak er null", vedtakFattet);
-        assertEquals(todaysDate, vedtakFattet.getUtkastSistOppdatert());
-        assertNotNull("Vedtak fattet dato er null", utkastVedtak.getVedtakFattet());
-
+    private void prepareDatabasen(String versjonPrefix) {
+        DbTestUtils.testMigrate(postgresContainer.getDataSource(), versjonPrefix);
     }
 
     @AfterEach
@@ -74,16 +80,5 @@ public class FlywayDbMigrationTest {
                 postgresContainer.stopContainer();
             }
         }));
-    }
-
-    private void prepareDatabasen(String versjonPrefix) {
-        db = postgresContainer.getDb();
-
-        boolean isDbNull = db != null && db.getDataSource() != null;
-        if (isDbNull) {
-            transactor = new TransactionTemplate(new DataSourceTransactionManager(db.getDataSource()));
-        }
-
-        DbTestUtils.testMigrate(postgresContainer.getDataSource(), versjonPrefix);
     }
 }
