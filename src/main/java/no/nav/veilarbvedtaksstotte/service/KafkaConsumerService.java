3d195ad9e03f4bcadb51d379207e0888a3a96c48
matiasvinjevoll <matias.vinjevoll@nav.no>
2021-03-23 14:54:42 +0100

    Løs opp i syklisk avhengighet for beans, take 2

--- src/main/java/no/nav/veilarbvedtaksstotte/service/KafkaConsumerService.java
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/KafkaConsumerService.java
@@ -3,13 +3,17 @@ package no.nav.veilarbvedtaksstotte.service;
 import no.nav.veilarbvedtaksstotte.kafka.dto.KafkaAvsluttOppfolging;
 import no.nav.veilarbvedtaksstotte.kafka.dto.KafkaOppfolgingsbrukerEndring;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
+    private final VedtakService vedtakService;
 
     @Autowired
-    private VedtakService vedtakService;
+    public KafkaConsumerService(@Lazy VedtakService vedtakService) {
+        this.vedtakService = vedtakService;
+    }
 
     public void behandleEndringPaAvsluttOppfolging(KafkaAvsluttOppfolging kafkaAvsluttOppfolging) {
         vedtakService.behandleAvsluttOppfolging(kafkaAvsluttOppfolging);
