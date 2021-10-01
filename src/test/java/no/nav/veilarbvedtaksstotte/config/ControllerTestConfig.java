d195a64018187bf68b767d310dd1e182d7dc119c
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2021-10-01 21:22:57 +0200

    TC-40 legg til api endepunkt for uthenting av kodeverk for innsatsgruppe(kode,beskrivelse,arenakode)

--- src/test/java/no/nav/veilarbvedtaksstotte/config/ControllerTestConfig.java
+++ src/test/java/no/nav/veilarbvedtaksstotte/config/ControllerTestConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
         UtkastController.class,
         UtrullingController.class,
         AdminController.class,
-        InnsatsbehovController.class
+        InnsatsbehovController.class,
+        KodeverkController.class
 })
 public class ControllerTestConfig {}
