4fc2ac16edfefc7f492b0a2252ad264ba5fcf130
stianStensli <stian.horn.stensli@nav.no>
2022-06-14 09:22:31 +0200

    fjerner ubrukt import

--- src/main/java/no/nav/veilarbvedtaksstotte/service/DokumentService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/DokumentService.kt
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.util.*
-import java.util.function.Supplier
 
 @Service
 class DokumentService(
