b3161dc077b6f6defe26d673584a902f0f645817
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-09-21 10:35:45 +0200

    Skriv om tester slik at de kjører Noe i testoppsettet gjorde at testene ikke kjørte likt med Maven vs IntelliJ

--- src/test/java/no/nav/veilarbvedtaksstotte/kafka/KafkaVedtakStatusEndringConsumerTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/kafka/KafkaVedtakStatusEndringConsumerTest.kt
@@ -1,41 +1,27 @@
 package no.nav.veilarbvedtaksstotte.kafka
 
 import com.ninjasquad.springmockk.MockkBean
-import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import com.ninjasquad.springmockk.SpykBean
 import io.mockk.every
+import io.mockk.verify
 import no.nav.common.types.identer.AktorId
-import no.nav.pto_schema.kafka.avro.Vedtak14aFattetDvh
-import no.nav.veilarbvedtaksstotte.config.ApplicationTestConfig
 import no.nav.veilarbvedtaksstotte.config.KafkaConfig
 import no.nav.veilarbvedtaksstotte.config.KafkaProperties
 import no.nav.veilarbvedtaksstotte.domain.kafka.KafkaVedtakStatusEndring
-import no.nav.veilarbvedtaksstotte.domain.vedtak.Hovedmal
-import no.nav.veilarbvedtaksstotte.domain.vedtak.Innsatsgruppe
-import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak
-import no.nav.veilarbvedtaksstotte.repository.VedtaksstotteRepository
+import no.nav.veilarbvedtaksstotte.service.KafkaVedtakStatusEndringConsumer
 import no.nav.veilarbvedtaksstotte.service.UnleashService
-import no.nav.veilarbvedtaksstotte.utils.KafkaTestUtils
-import no.nav.veilarbvedtaksstotte.utils.TestData
+import no.nav.veilarbvedtaksstotte.utils.AbstractVedtakIntegrationTest
 import no.nav.veilarbvedtaksstotte.utils.TestUtils
 import no.nav.veilarbvedtaksstotte.utils.toJson
 import org.apache.commons.lang3.RandomStringUtils
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.KafkaContainer
 import java.time.LocalDateTime
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicReference
 
-@SpringBootTest(classes = [ApplicationTestConfig::class])
-@ActiveProfiles("local")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-class KafkaVedtakStatusEndringConsumerTest {
+class KafkaVedtakStatusEndringConsumerTest : AbstractVedtakIntegrationTest() {
 
     @Autowired
     lateinit var kafkaProperties: KafkaProperties
@@ -49,23 +35,20 @@ class KafkaVedtakStatusEndringConsumerTest {
     @Autowired
     lateinit var kafkaAvroContext: KafkaConfig.KafkaAvroContext
 
-    @Autowired
-    lateinit var jdbcTemplate: JdbcTemplate
-
-    @Autowired
-    lateinit var vedtakRepository: VedtaksstotteRepository
-
     @MockkBean
     lateinit var unleashService: UnleashService
 
+    @SpykBean
+    lateinit var kafkaVedtakStatusEndringConsumer: KafkaVedtakStatusEndringConsumer
+
     @Test
-    fun `konsumerer melding om statusendring som fører til publisering av melding pa topic for rapportering til DVH`() {
+    fun `konsumerer melding om statusendring`() {
 
         every {
             unleashService.isRapporterDvhAsynkront
         } returns true
 
-        val vedtak = gittFattetVedtakDer(
+        val vedtak = lagreFattetVedtak(
             aktorId = AktorId.of(RandomStringUtils.randomNumeric(10)),
             vedtakFattetDato = LocalDateTime.now()
         )
@@ -82,48 +65,18 @@ class KafkaVedtakStatusEndringConsumerTest {
             ProducerRecord(kafkaProperties.vedtakStatusEndringTopic, "key", statusEndring.toJson())
         )
 
-        val konsumerteMeldinger: AtomicReference<MutableMap<String, Vedtak14aFattetDvh>> =
-            AtomicReference(mutableMapOf())
-
-        val testConsumer = KafkaTestUtils.testConsumer<Any>(
-            brokerUrl = kafkaContainer.bootstrapServers,
-            topicName = kafkaProperties.vedtakFattetDvhTopic,
-            valueDeserializer = KafkaAvroDeserializer(null, kafkaAvroContext.config)
-        ) { record ->
-            konsumerteMeldinger.get()[record.key()] = record.value() as Vedtak14aFattetDvh
-        }
-
-        testConsumer.start()
-
         TestUtils.verifiserAsynkront(
             10, TimeUnit.SECONDS
         ) {
-            val konsumertMelding = konsumerteMeldinger.get()[vedtak.aktorId]
-
-            assertNotNull(konsumertMelding)
+            verify {
+                kafkaVedtakStatusEndringConsumer.konsumer(match {
+                    it.value() == KafkaVedtakStatusEndring()
+                        .setVedtakId(statusEndring.vedtakId)
+                        .setAktorId(statusEndring.aktorId)
+                        .setVedtakStatusEndring(statusEndring.vedtakStatusEndring)
+                        .setTimestamp(statusEndring.timestamp)
+                })
+            }
         }
-    }
-
-    private fun gittFattetVedtakDer(
-        aktorId: AktorId,
-        vedtakFattetDato: LocalDateTime
-    ): Vedtak {
-        vedtakRepository.opprettUtkast(
-            aktorId.get(),
-            TestData.TEST_VEILEDER_IDENT,
-            TestData.TEST_OPPFOLGINGSENHET_ID
-        )
-        val vedtak = vedtakRepository.hentUtkast(aktorId.get())
-        vedtak.innsatsgruppe = Innsatsgruppe.SITUASJONSBESTEMT_INNSATS
-        vedtak.hovedmal = Hovedmal.BEHOLDE_ARBEID
-        vedtakRepository.oppdaterUtkast(vedtak.id, vedtak)
-        vedtakRepository.ferdigstillVedtak(vedtak.id)
-        jdbcTemplate.update(
-            "UPDATE VEDTAK SET VEDTAK_FATTET = ? WHERE ID = ?",
-            vedtakFattetDato,
-            vedtak.id
-        )
-
-        return vedtakRepository.hentVedtak(vedtak.id)
     }
 }
