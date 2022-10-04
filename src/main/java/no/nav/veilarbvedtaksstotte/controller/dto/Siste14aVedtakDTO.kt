1114ceb6bc52e7164ec14bda949bb28d327beb1b
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-10-04 12:36:08 +0200

    Utvid siste 14a API Legg til resterende felter (bortsett fra aktorId) slik at det samsvarer med Kafka-meldinger

--- src/main/java/no/nav/veilarbvedtaksstotte/controller/dto/Siste14aVedtakDTO.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/controller/dto/Siste14aVedtakDTO.kt
@@ -2,16 +2,21 @@ package no.nav.veilarbvedtaksstotte.controller.dto
 
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Siste14aVedtak
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Innsatsgruppe
+import java.time.ZonedDateTime
 
 data class Siste14aVedtakDTO(
     val innsatsgruppe: Innsatsgruppe,
-    val hovedmal: Siste14aVedtak.HovedmalMedOkeDeltakelse?
+    val hovedmal: Siste14aVedtak.HovedmalMedOkeDeltakelse?,
+    val fattetDato: ZonedDateTime,
+    val fraArena: Boolean
 ) {
     companion object {
         fun fraSiste14aVedtak(siste14aVedtak: Siste14aVedtak): Siste14aVedtakDTO {
             return Siste14aVedtakDTO(
                 innsatsgruppe = siste14aVedtak.innsatsgruppe,
-                hovedmal = siste14aVedtak.hovedmal
+                hovedmal = siste14aVedtak.hovedmal,
+                fattetDato = siste14aVedtak.fattetDato,
+                fraArena = siste14aVedtak.fraArena
             )
         }
     }
