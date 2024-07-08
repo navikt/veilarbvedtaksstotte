842c47556ec113e31038797267085f2dded14f03
Dragutin Vujovic <dragutin.vujovic@nav.no>
2024-07-08 14:08:20 +0200

    Remove abac environment variables

--- src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.kt
@@ -1,7 +1,6 @@
 package no.nav.veilarbvedtaksstotte.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 
 @ConfigurationProperties(prefix = "app.env")
 data class EnvironmentProperties (
@@ -9,7 +8,6 @@ data class EnvironmentProperties (
     val veilarbloginOpenAmClientId: String,
     val stsDiscoveryUrl: String,
     val openAmRefreshUrl: String,
-    val abacUrl: String,
     val dbUrl: String,
     val unleashUrl: String,
     val unleashApiToken: String,
