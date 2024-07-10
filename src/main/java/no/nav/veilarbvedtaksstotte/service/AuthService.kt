a742e7ce140193cfe1c424373d7e9a0460b4cb84
Dragutin Vujovic <dragutin.vujovic@nav.no>
2024-07-10 10:16:48 +0200

    Fix tilgang for beslutter oversikt

--- src/main/java/no/nav/veilarbvedtaksstotte/service/AuthService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/AuthService.kt
@@ -115,7 +115,7 @@ class AuthService(
                         NavAnsattTilgangTilEksternBrukerPolicyInput(
                             hentInnloggetVeilederUUID(), TilgangType.SKRIVE, it
                         )
-                    ).getOrThrow().isPermit
+                    ).map { decision -> decision.isPermit }.getOrDefault(false)
                 )
             }
         }
