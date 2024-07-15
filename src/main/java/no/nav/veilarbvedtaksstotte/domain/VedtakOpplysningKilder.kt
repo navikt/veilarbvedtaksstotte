e5434de9239e40e5a037721616b43fa01d27d39d
Dragutin Vujovic <66312566+dragutin-nav@users.noreply.github.com>
2024-07-15 13:18:20 +0200

    Pdfgen to gcp (#560)
    * Move pto-pdfgen to gcp

--- src/main/java/no/nav/veilarbvedtaksstotte/domain/VedtakOpplysningKilder.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/domain/VedtakOpplysningKilder.kt
@@ -1,7 +1,7 @@
 package no.nav.veilarbvedtaksstotte.domain
 
 enum class VedtakOpplysningKilder(val desc: String) {
-    CV("CV-en/jobbønskene dine på nav.no"),
+    CV("CV-en"),
     REGISTRERING("Svarene dine fra da du registrerte deg"),
     EGENVURDERING("Svarene dine om behov for veiledning")
 }
