c43f0cbc801973ca053edce4724af79e6794cbaa
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-09-13 13:13:57 +0200

    Slett referanser til veilarblogin og openAM

--- src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/config/EnvironmentProperties.kt
@@ -4,24 +4,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "app.env")
 data class EnvironmentProperties (
-    val stsDiscoveryUrl: String,
+    val aiaBackendUrl: String,
     val dbUrl: String,
-    val unleashUrl: String,
-    val unleashApiToken: String,
-    val norg2Url: String,
     val naisAadDiscoveryUrl: String,
     val naisAadClientId: String,
     val naisAadIssuer: String,
+    val norg2Url: String,
     val poaoTilgangUrl: String,
     val poaoTilgangScope: String,
-    val aiaBackendUrl: String,
-    val tokenxDiscoveryUrl: String,
-    val tokenxClientId: String,
-    val veilarboppfolgingUrl: String,
-    val veilarboppfolgingScope: String,
-    val safUrl: String,
     val safScope: String,
-    val veilarbveilederUrl: String,
-    val veilarbveilederScope: String
+    val safUrl: String,
+    val stsDiscoveryUrl: String,
+    val tokenxClientId: String,
+    val tokenxDiscoveryUrl: String,
+    val unleashApiToken: String,
+    val unleashUrl: String,
+    val veilarboppfolgingScope: String,
+    val veilarboppfolgingUrl: String,
+    val veilarbveilederScope: String,
+    val veilarbveilederUrl: String
 
 )
