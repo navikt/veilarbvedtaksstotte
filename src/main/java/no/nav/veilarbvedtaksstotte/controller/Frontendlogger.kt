08a6104fc5ec5ba92877a5a9661749c4dd527c94
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-10-10 15:06:56 +0200

    Kommenter ut LoggerFactory

--- src/main/java/no/nav/veilarbvedtaksstotte/controller/Frontendlogger.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/controller/Frontendlogger.kt
@@ -2,9 +2,6 @@ package no.nav.veilarbvedtaksstotte.controller
 
 import no.nav.common.metrics.Event
 import no.nav.common.metrics.MetricsClient
-import no.nav.common.utils.EnvironmentUtils
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/logger")
 class Frontendlogger(val metricsClient: MetricsClient) {
-    val log: Logger = LoggerFactory.getLogger(Frontendlogger::class.java)
+//    val log: Logger = LoggerFactory.getLogger(Frontendlogger::class.java)
 
     @PostMapping("/event")
     fun skrivEventTilInflux(@RequestBody event: FrontendEvent) {
