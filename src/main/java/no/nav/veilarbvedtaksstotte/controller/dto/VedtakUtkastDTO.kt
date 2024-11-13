fb1470bab59e0fd2d82281971ca50e7159599431
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-13 18:28:03 +0100

    Legg til Swagger-dokumentasjon for "VedtakUtkastDTO"

--- src/main/java/no/nav/veilarbvedtaksstotte/controller/dto/VedtakUtkastDTO.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/controller/dto/VedtakUtkastDTO.kt
@@ -1,22 +1,36 @@
 package no.nav.veilarbvedtaksstotte.controller.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.veilarbvedtaksstotte.domain.vedtak.BeslutterProsessStatus
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Hovedmal
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Innsatsgruppe
 import java.time.LocalDateTime
 
 data class VedtakUtkastDTO(
+    @Schema(description = "ID-en til vedtaket")
     val id: Long? = null,
+    @Schema(description = "Hovedmålet med oppfølgingen")
     val hovedmal: Hovedmal? = null,
+    @Schema(description = "Innsatsgruppen som brukeren som vedtaket gjelder for kvalifiserer til")
     val innsatsgruppe: Innsatsgruppe? = null,
+    @Schema(description = "Dato og tid for når vedtaket sist ble endret")
     val utkastSistOppdatert: LocalDateTime? = null,
+    @Schema(description = "Begrunnelsen som ligger til grunn for oppfølgingen (innsatsgruppen) som brukeren vil ha krav på")
     val begrunnelse: String? = null,
+    @Schema(description = "Identen til veilederen som er ansvarlig for vedtaket")
     val veilederIdent: String? = null,
+    @Schema(description = "Navnet på veilederen som er ansvarlig for vedtaket")
     val veilederNavn: String? = null,
+    @Schema(description = "ID-en til oppfølgingsenheten til brukeren som vedtaket gjelder for")
     val oppfolgingsenhetId: String? = null,
+    @Schema(description = "Navnet på oppfølgingsenheten til brukeren som vedtaket gjelder for")
     val oppfolgingsenhetNavn: String? = null,
+    @Schema(description = "Identen til beslutteren som er ansvarlig for vedtaket")
     val beslutterIdent: String? = null,
+    @Schema(description = "Navnet på beslutteren som er ansvarlig for vedtaket")
     val beslutterNavn: String? = null,
+    @Schema(description = "Opplysninger (kilder) som er vektlagt i vedtaket")
     val opplysninger: List<String?>? = null,
+    @Schema(description = "Nåværende steg i beslutterprosessen for vedtaket")
     val beslutterProsessStatus: BeslutterProsessStatus? = null,
 )
