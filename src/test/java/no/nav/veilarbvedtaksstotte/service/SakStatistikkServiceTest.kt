d989cc108942bb41485a3bf0d5ac53fd68c5d577
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-12-20 09:17:46 +0100

    Committer ny testklasse for å se om testene kjører på Github. Endrer denne for å se om det virker.
    Co-authored-by: Julie Hill Roa <julie.hill.roa@nav.no>

--- src/test/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkServiceTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkServiceTest.kt
@@ -3,16 +3,20 @@ package no.nav.veilarbvedtaksstotte.service
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.mockito.Mockito.reset
+import org.mockito.MockitoAnnotations
 
 class SakStatistikkServiceTest   {
 
     companion object {
+        @Mock
         lateinit var sakStatistikkService: SakStatistikkService
     }
 
     @BeforeEach
     fun before() {
+        MockitoAnnotations.openMocks(this)
         reset(sakStatistikkService)
     }
     @Test
