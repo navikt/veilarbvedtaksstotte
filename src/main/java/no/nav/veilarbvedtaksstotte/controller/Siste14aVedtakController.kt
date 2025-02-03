1b4ef762cb0e5d969ea16980200f0327b5d905e1
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-02-03 19:21:00 +0100

    Signaliser at deprekert endepunkt ikkje lenger kan brukast

--- src/main/java/no/nav/veilarbvedtaksstotte/controller/Siste14aVedtakController.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/controller/Siste14aVedtakController.kt
@@ -2,10 +2,6 @@ package no.nav.veilarbvedtaksstotte.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import no.nav.common.types.identer.Fnr
-import no.nav.poao_tilgang.client.TilgangType
-import no.nav.veilarbvedtaksstotte.controller.dto.Siste14aVedtakDTO
-import no.nav.veilarbvedtaksstotte.service.AuthService
-import no.nav.veilarbvedtaksstotte.service.Siste14aVedtakService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,27 +15,11 @@ import org.springframework.web.server.ResponseStatusException
     name = "Siste § 14 a-vedtak",
     description = "Funksjonalitet knyttet til siste § 14 a-vedtak."
 )
-class Siste14aVedtakController(
-    val authService: AuthService,
-    val siste14aVedtakService: Siste14aVedtakService
-    ) {
+class Siste14aVedtakController {
 
-    @Deprecated("Ikke lenger i bruk pga personvernstiltak", ReplaceWith("v2 av samme endepunkt"))
+    @Deprecated("Ikke lenger i bruk pga personvernstiltak", ReplaceWith("/api/v2/hent-siste-14a-vedtak"))
     @GetMapping("/siste-14a-vedtak")
-    fun hentSiste14aVedtak(@RequestParam("fnr") fnr: Fnr): Siste14aVedtakDTO? {
-        sjekkTilgang(fnr)
-
-        return siste14aVedtakService.siste14aVedtak(fnr)
-            ?.let { Siste14aVedtakDTO.fraSiste14aVedtak(it) }
-    }
-
-    private fun sjekkTilgang(fnr: Fnr) {
-        if (authService.erSystemBruker()) {
-            if (!authService.harSystemTilSystemTilgangMedEkstraRolle("siste-14a-vedtak")) {
-                throw ResponseStatusException(HttpStatus.FORBIDDEN);
-            }
-        } else {
-            authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
-        }
+    fun hentSiste14aVedtak(@RequestParam("fnr") fnr: Fnr) {
+        throw ResponseStatusException(HttpStatus.GONE)
     }
 }
--- src/test/java/no/nav/veilarbvedtaksstotte/controller/Siste14aVedtakControllerTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/controller/Siste14aVedtakControllerTest.kt
@@ -1,110 +1,28 @@
 package no.nav.veilarbvedtaksstotte.controller
 
-import com.ninjasquad.springmockk.MockkBean
-import io.mockk.every
 import no.nav.common.types.identer.Fnr
-import no.nav.poao_tilgang.client.TilgangType
 import no.nav.veilarbvedtaksstotte.service.AuthService
-import no.nav.veilarbvedtaksstotte.service.Siste14aVedtakService
 import org.apache.commons.lang3.RandomStringUtils
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
-import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.web.server.ResponseStatusException
 
 @WebMvcTest(Siste14aVedtakController::class)
-@Import(AuthService::class)
 class Siste14aVedtakControllerTest {
-
-    @MockkBean
-    lateinit var authService: AuthService
-
-    @MockkBean
-    lateinit var siste14aVedtakService: Siste14aVedtakService
-
     @Autowired
     lateinit var mockMvc: MockMvc
 
     val fnr = Fnr(RandomStringUtils.randomNumeric(11))
 
-    @BeforeEach
-    fun beforeEach() {
-        every {
-            siste14aVedtakService.siste14aVedtak(fnr)
-        } returns null
-    }
-
     @Test
-    fun `gir tilgang til systembruker med rolle siste-14a-vedtak`() {
-
-        every {
-            authService.erSystemBruker()
-        } returns true
-
-        every {
-            authService.harSystemTilSystemTilgangMedEkstraRolle("siste-14a-vedtak")
-        } returns true
-
+    fun `request mot siste-14a-vedtak skal gi HTTP 410 Gone`() {
         val response = mockMvc.perform(MockMvcRequestBuilders.get("/api/siste-14a-vedtak").queryParam("fnr", fnr.get()))
             .andReturn().response
 
-        assertEquals(200, response.status.toLong())
-    }
-
-    @Test
-    fun `gir ikke tilgang til systembruker uten rolle siste-14a-vedtak`() {
-
-        every {
-            authService.erSystemBruker()
-        } returns true
-
-        every {
-            authService.harSystemTilSystemTilgangMedEkstraRolle("siste-14a-vedtak")
-        } returns false
-
-        val response = mockMvc.perform(MockMvcRequestBuilders.get("/api/siste-14a-vedtak").queryParam("fnr", fnr.get()))
-            .andReturn().response
-
-        assertEquals(403, response.status.toLong())
-    }
-
-    @Test
-    fun `gir tilgang hvis ikke systembruker og tilgang til bruker`() {
-
-        every {
-            authService.erSystemBruker()
-        } returns false
-
-        every {
-            authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
-        } answers { }
-
-        val response = mockMvc.perform(MockMvcRequestBuilders.get("/api/siste-14a-vedtak").queryParam("fnr", fnr.get()))
-            .andReturn().response
-
-        assertEquals(200, response.status.toLong())
-    }
-
-    @Test
-    fun `gir ikke tilgang hvis ikke systembruker og ikke tilgang til bruker`() {
-
-        every {
-            authService.erSystemBruker()
-        } returns false
-
-        every {
-            authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
-        } throws ResponseStatusException(HttpStatus.FORBIDDEN)
-
-        val response = mockMvc.perform(MockMvcRequestBuilders.get("/api/siste-14a-vedtak").queryParam("fnr", fnr.get()))
-            .andReturn().response
-
-        assertEquals(403, response.status.toLong())
+        assertEquals(410, response.status.toLong())
     }
 }
