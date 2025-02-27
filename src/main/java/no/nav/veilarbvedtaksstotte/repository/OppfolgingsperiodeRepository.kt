cc33cd7bf6963b9d08f8621cfdc8b8222981e81f
JulieHillRoa <julie.hill.roa@nav.no>
2025-02-27 23:12:51 +0100

    sender inn riktige verdier i insert sqlsetning

--- src/main/java/no/nav/veilarbvedtaksstotte/repository/OppfolgingsperiodeRepository.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/repository/OppfolgingsperiodeRepository.kt
@@ -1,16 +1,13 @@
 package no.nav.veilarbvedtaksstotte.repository
 
 import no.nav.veilarbvedtaksstotte.domain.kafka.KafkaOppfolgingsperiode
+import no.nav.veilarbvedtaksstotte.utils.SecureLog.secureLog
 import no.nav.veilarbvedtaksstotte.utils.TimeUtils
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
 @Repository
 class OppfolgingsperiodeRepository(val jdbcTemplate: JdbcTemplate) {
-
-    private val log: Logger = LoggerFactory.getLogger(SakStatistikkRepository::class.java)
 
     val OPPFOLGINGSPERIODE_TABLE = "OPPFOLGINGSPERIODE"
     val OPPFOLGINGSPERIODEUUID = "OPPFOLGINGSPERIODE_ID"
@@ -22,14 +19,24 @@ class OppfolgingsperiodeRepository(val jdbcTemplate: JdbcTemplate) {
     fun insertOppfolgingsperiode(oppfolgingsperiode: KafkaOppfolgingsperiode) {
         val sql =
             """
-                INSERT INTO $OPPFOLGINGSPERIODE_TABLE($OPPFOLGINGSPERIODEUUID,$AKTORID,$STARTDATO,$SLUTTDATO,$STARTET_BEGRUNNELSE)
+                INSERT INTO $OPPFOLGINGSPERIODE_TABLE ($OPPFOLGINGSPERIODEUUID, $AKTORID, $STARTDATO, $SLUTTDATO, $STARTET_BEGRUNNELSE)
                 VALUES (?, ?, ?, ?, ?)
             """
         try {
-            jdbcTemplate.update(sql, oppfolgingsperiode.uuid, oppfolgingsperiode.aktorId, TimeUtils.toTimestampOrNull(oppfolgingsperiode.startDato?.toInstant()),
-                TimeUtils.toTimestampOrNull(oppfolgingsperiode.sluttDato?.toInstant()), oppfolgingsperiode.startetBegrunnelse)
+            jdbcTemplate.update(
+                sql,
+                oppfolgingsperiode.uuid,
+                oppfolgingsperiode.aktorId,
+                TimeUtils.toTimestampOrNull(oppfolgingsperiode.startDato?.toInstant()),
+                TimeUtils.toTimestampOrNull(oppfolgingsperiode.sluttDato?.toInstant()),
+                oppfolgingsperiode.startetBegrunnelse.name
+            )
         } catch (e: Exception) {
-            log.error("Kunne ikke lagre oppfølgingsperiode, feil: {} , oppfolgingsperiode: {}", e, oppfolgingsperiode)
+            secureLog.error(
+                "Kunne ikke lagre oppfølgingsperiode, feil: {} , oppfolgingsperiode: {}",
+                e,
+                oppfolgingsperiode
+            )
         }
     }
 }
