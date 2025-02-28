dcbbb434fdb7abe77eaf33afcf418cdf2355e537
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-02-28 10:59:46 +0100

    Legg til TODO-kommentar om vidare steg

--- src/main/java/no/nav/veilarbvedtaksstotte/service/BrukerIdenterService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/BrukerIdenterService.kt
@@ -18,8 +18,12 @@ class BrukerIdenterService {
     fun behandlePdlAktorV2Melding(aktorRecord: ConsumerRecord<AktorId, Aktor>) {
         logger.info("Konsumerte melding på pdl.aktor-v2 topic")
 
-        /*
-            1.
-         */
+        // 2025-02-28: TODO Implementere referanseimplementasjonen til PDL
+        // Sjå https://pdl-docs.ansatt.nav.no/ekstern/index.html#identitetshendelser_pa_kafka
+        //
+        // "Som konsument av denne topicen for bygging av egen cache anbefales følgende fremgangsmåte: Når en melding
+        // mottas, henter man ut alle identer fra meldingen. For hver av disse identene, finner man alle tilknyttede
+        // personer i egen cache. For hver av disse personene, sletter man alle identer. Deretter lagrer man identene i
+        // den mottatte meldingen som én person."
     }
 }
