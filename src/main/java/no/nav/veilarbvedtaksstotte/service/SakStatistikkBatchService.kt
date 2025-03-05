a69bb953624b4d822ba2d5859c21b3bcabb30643
Mathias S. Müller <mathias.sundt.muller@nav.no>
2025-03-05 11:14:54 +0100

    Lag funksjon for å laste fattede vedtak inn i sakstatistikk-tabellen
    Co-authored-by: Sneha Desai <sneha.desai@nav.no>
    Co-authored-by: Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>

---
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkBatchService.kt
@@ -0,0 +1,24 @@
+package no.nav.veilarbvedtaksstotte.service
+
+import no.nav.common.types.identer.Fnr
+import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak
+import no.nav.veilarbvedtaksstotte.repository.VedtaksstotteRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
+class SakStatistikkBatchService @Autowired constructor(
+    private val vedtaksstotteRepository: VedtaksstotteRepository,
+    private val sakStatistikkService: SakStatistikkService,
+    private val authService: AuthService
+) {
+    // TODO: Endre cron til å kjøre på riktig tidspunkt
+    @Scheduled(cron = "0 30 11 5 3 *") // Kjører 5. mars kl 11:30
+    fun lastInnFattedeVedtak(){
+        val fattedeVedtak: List<Vedtak> = vedtaksstotteRepository.hentFattedeVedtak(10000, 0)
+        fattedeVedtak.forEach { vedtak ->
+            val brukerFnr: Fnr = authService.getFnrOrThrow(vedtak.aktorId)
+            sakStatistikkService.fattetVedtak(vedtak, brukerFnr) }
+    }
+}
