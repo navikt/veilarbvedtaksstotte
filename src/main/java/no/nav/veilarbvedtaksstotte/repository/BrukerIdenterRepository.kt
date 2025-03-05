f09e4a90bfc0a4b5dc7270239da688327392a46c
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-03-05 10:59:41 +0100

    Implementer støtte for å behandle tombstones

--- src/main/java/no/nav/veilarbvedtaksstotte/repository/BrukerIdenterRepository.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/repository/BrukerIdenterRepository.kt
@@ -1,5 +1,6 @@
 package no.nav.veilarbvedtaksstotte.repository
 
+import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.Id
 import no.nav.veilarbvedtaksstotte.domain.IdentDetaljer
 import no.nav.veilarbvedtaksstotte.domain.PersonNokkel
@@ -63,5 +64,10 @@ class BrukerIdenterRepository(
         val sql = "SELECT nextval('BRUKER_IDENTER_PERSON_SEQ')"
         return jdbcTemplate.queryForObject(sql, PersonNokkel::class.java)
             ?: throw IllegalStateException("Kunne ikke hente ny verdi fra \"BRUKER_IDENTER_PERSON_SEQ\" sekvensen.")
+    }
+
+    fun slett(aktorRecordKey: AktorId) {
+        val sql = "DELETE FROM bruker_identer WHERE ident = ?"
+        jdbcTemplate.update(sql, aktorRecordKey.get())
     }
 }
