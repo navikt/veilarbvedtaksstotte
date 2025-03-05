92f488c3ea57024023356ab456321103b34a1909
Sneha Desai <sneha.desai@nav.no>
2025-03-05 13:01:41 +0100

    La til info logger til SakStatistikkBatchService
    Co-authored-by: Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>

--- src/main/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkBatchService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkBatchService.kt
@@ -3,6 +3,8 @@ package no.nav.veilarbvedtaksstotte.service
 import no.nav.common.types.identer.Fnr
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Vedtak
 import no.nav.veilarbvedtaksstotte.repository.VedtaksstotteRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -13,12 +15,20 @@ class SakStatistikkBatchService @Autowired constructor(
     private val sakStatistikkService: SakStatistikkService,
     private val authService: AuthService
 ) {
+    private val log: Logger = LoggerFactory.getLogger(SakStatistikkBatchService::class.java)
     // TODO: Endre cron til å kjøre på riktig tidspunkt
-    @Scheduled(cron = "0 30 12 5 3 *") // Kjører 5. mars kl 11:30
+    @Scheduled(cron = "0 15 13 5 3 *") // Kjører 5. mars kl 11:30
     fun lastInnFattedeVedtak(){
+        log.info("Klar for å laste inn fattede Vedtak")
         val fattedeVedtak: List<Vedtak> = vedtaksstotteRepository.hentFattedeVedtak(10000, 0)
+        log.info(String.format("Antall fattedeVedtak: {}"), fattedeVedtak.size)
         fattedeVedtak.forEach { vedtak ->
-            val brukerFnr: Fnr = authService.getFnrOrThrow(vedtak.aktorId)
-            sakStatistikkService.fattetVedtak(vedtak, brukerFnr) }
+            try {
+                val brukerFnr: Fnr = authService.getFnrOrThrow(vedtak.aktorId)
+                sakStatistikkService.fattetVedtak(vedtak, brukerFnr)
+            } catch(e: Exception) {
+                log.error("Feil med ident:{}, Feil:{}", vedtak.aktorId, e)
+            }
+        }
     }
 }
