40034d7b05ba32cf3998fa1afb49a2282fb8cdeb
JulieHillRoa <julie.hill.roa@nav.no>
2025-03-12 16:43:09 +0100

    implementert hentGjeldende14aVedtak
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- src/main/java/no/nav/veilarbvedtaksstotte/domain/vedtak/Gjeldende14aVedtak.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/domain/vedtak/Gjeldende14aVedtak.kt
@@ -5,8 +5,16 @@ import java.time.ZonedDateTime
 
 data class Gjeldende14aVedtak(
     val aktorId: AktorId,
-    val vedtakId: String,
+    val vedtakId: String?, //TODO hente verdi på ordentlig her
     val innsatsgruppe: Innsatsgruppe,
     val hovedmal: HovedmalMedOkeDeltakelse?,
     val fattetDato: ZonedDateTime,
+)
+
+fun Siste14aVedtak.toGjeldende14aVedtak(): Gjeldende14aVedtak = Gjeldende14aVedtak(
+    aktorId = aktorId,
+    vedtakId = null, //TODO hente verdi på ordentlig her
+    innsatsgruppe = innsatsgruppe,
+    hovedmal = hovedmal,
+    fattetDato = fattetDato
 )
