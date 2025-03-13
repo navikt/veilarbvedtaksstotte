c304c1b7f9af6769a35b333414e04564e3b83651
JulieHillRoa <julie.hill.roa@nav.no>
2025-03-13 14:23:10 +0100

    Rydde opp vedtakId på gjeldende14aVedtak

--- src/main/java/no/nav/veilarbvedtaksstotte/domain/vedtak/Gjeldende14aVedtak.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/domain/vedtak/Gjeldende14aVedtak.kt
@@ -5,7 +5,6 @@ import java.time.ZonedDateTime
 
 data class Gjeldende14aVedtak(
     val aktorId: AktorId,
-    val vedtakId: String?, //TODO hente verdi på ordentlig her
     val innsatsgruppe: Innsatsgruppe,
     val hovedmal: HovedmalMedOkeDeltakelse?,
     val fattetDato: ZonedDateTime,
@@ -13,7 +12,6 @@ data class Gjeldende14aVedtak(
 
 fun Siste14aVedtak.toGjeldende14aVedtak(): Gjeldende14aVedtak = Gjeldende14aVedtak(
     aktorId = aktorId,
-    vedtakId = null, //TODO hente verdi på ordentlig her
     innsatsgruppe = innsatsgruppe,
     hovedmal = hovedmal,
     fattetDato = fattetDato
