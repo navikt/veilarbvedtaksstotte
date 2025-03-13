f6815e83fda58b1cb0cc4c6ca7000b541584ef1e
Ingrid Fosså <ingrid.fossa@nav.no>
2025-03-13 15:54:27 +0100

    WIP

--- src/test/java/no/nav/veilarbvedtaksstotte/controller/Gjeldende14avedtakControllerTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/controller/Gjeldende14avedtakControllerTest.kt
@@ -3,10 +3,15 @@ package no.nav.veilarbvedtaksstotte.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.Fnr
 import no.nav.poao_tilgang.client.TilgangType
+import no.nav.veilarbvedtaksstotte.controller.dto.toGjeldende14aVedtakDto
 import no.nav.veilarbvedtaksstotte.controller.v2.dto.Gjeldende14aVedtakRequest
 import no.nav.veilarbvedtaksstotte.controller.v2.dto.Siste14aVedtakRequest
+import no.nav.veilarbvedtaksstotte.domain.vedtak.Gjeldende14aVedtak
+import no.nav.veilarbvedtaksstotte.domain.vedtak.HovedmalMedOkeDeltakelse
+import no.nav.veilarbvedtaksstotte.domain.vedtak.Innsatsgruppe
 import no.nav.veilarbvedtaksstotte.service.AuthService
 import no.nav.veilarbvedtaksstotte.service.Gjeldende14aVedtakService
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -20,6 +25,12 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.web.server.ResponseStatusException
+import java.time.ZonedDateTime
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
 
 @WebMvcTest(Gjeldende14aVedtakController::class)
 @Import(AuthService::class)
@@ -59,8 +70,9 @@ class Gjeldende14avedtakControllerTest {
 
         val response = mockMvc.perform(
             post("/api/hent-gjeldende-14a-vedtak")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(Gjeldende14aVedtakRequest(fnr))))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(Gjeldende14aVedtakRequest(fnr)))
+        )
             .andReturn().response
 
         assertEquals(200, response.status.toLong())
@@ -77,9 +89,11 @@ class Gjeldende14avedtakControllerTest {
             authService.harSystemTilSystemTilgangMedEkstraRolle("gjeldende-14a-vedtak")
         } returns false
 
-        val response = mockMvc.perform(post("/api/hent-gjeldende-14a-vedtak")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr))))
+        val response = mockMvc.perform(
+            post("/api/hent-gjeldende-14a-vedtak")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr)))
+        )
             .andReturn().response
 
         assertEquals(403, response.status.toLong())
@@ -100,9 +114,11 @@ class Gjeldende14avedtakControllerTest {
             authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
         } answers { }
 
-        val response = mockMvc.perform(post("/api/hent-gjeldende-14a-vedtak")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr))))
+        val response = mockMvc.perform(
+            post("/api/hent-gjeldende-14a-vedtak")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr)))
+        )
             .andReturn().response
 
         assertEquals(200, response.status.toLong())
@@ -123,9 +139,11 @@ class Gjeldende14avedtakControllerTest {
             authService.sjekkVeilederUtenModiarolleTilgangTilBruker(fnr = fnr)
         } answers { }
 
-        val response = mockMvc.perform(post("/api/ekstern/hent-gjeldende-14a-vedtak")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr))))
+        val response = mockMvc.perform(
+            post("/api/ekstern/hent-gjeldende-14a-vedtak")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr)))
+        )
             .andReturn().response
 
         assertEquals(200, response.status.toLong())
@@ -146,12 +164,84 @@ class Gjeldende14avedtakControllerTest {
             authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
         } throws ResponseStatusException(HttpStatus.FORBIDDEN)
 
-        val response = mockMvc.perform(post("/api/hent-gjeldende-14a-vedtak")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr))))
+        val response = mockMvc.perform(
+            post("/api/hent-gjeldende-14a-vedtak")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(Siste14aVedtakRequest(fnr)))
+        )
             .andReturn().response
 
         assertEquals(403, response.status.toLong())
+    }
+
+    @Test
+    fun `returnerer vedtak hvis tilgang`() {
+        // Given
+        every {
+            authService.erSystemBruker()
+        } returns false
+
+        every {
+            authService.erEksternBruker()
+        } returns false
+
+        every {
+            authService.sjekkVeilederTilgangTilBruker(tilgangType = TilgangType.LESE, fnr = fnr)
+        } answers { }
+
+        val gjeldende14aVedtak = Gjeldende14aVedtak(
+            aktorId = AktorId.of("1111111111111"),
+            innsatsgruppe = Innsatsgruppe.STANDARD_INNSATS,
+            hovedmal = HovedmalMedOkeDeltakelse.SKAFFE_ARBEID,
+            fattetDato = ZonedDateTime.now()
+        )
+
+        every { gjeldende14aVedtakService.hentGjeldende14aVedtak(fnr) } answers {
+            gjeldende14aVedtak
+        }
+
+
+        // Then
+        val expectedContent = """
+        {
+            "innsatsgruppe": "${gjeldende14aVedtak.innsatsgruppe}",
+            "hovedmal": "${gjeldende14aVedtak.hovedmal}",
+            "fattetDato": "${gjeldende14aVedtak.fattetDato}"
+        }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/hent-gjeldende-14a-vedtak")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(
+                    """
+                    {
+                        "fnr": "12345678900"
+                    }
+                    """.trimMargin()
+                )
+        ).andExpect(status().is(200))
+            .andExpect(content().json(expectedContent))
+
+    }
+
+    @Test
+    fun `Testar mappar mellom Gjeldende14aVedtak og tilhøyrande Dto`() {
+        // Given
+        val gjeldende14aVedtak = Gjeldende14aVedtak(
+            aktorId = AktorId.of("1111111111111"),
+            innsatsgruppe = Innsatsgruppe.STANDARD_INNSATS,
+            hovedmal = HovedmalMedOkeDeltakelse.SKAFFE_ARBEID,
+            fattetDato = ZonedDateTime.now()
+        )
+
+        // When
+        val gjeldende14aVedtakDTO = gjeldende14aVedtak.toGjeldende14aVedtakDto();
+
+        // Then
+        assertEquals(gjeldende14aVedtak.hovedmal, gjeldende14aVedtakDTO.hovedmal)
+        assertEquals(gjeldende14aVedtak.innsatsgruppe, gjeldende14aVedtakDTO.innsatsgruppe)
+        assertEquals(gjeldende14aVedtak.fattetDato, gjeldende14aVedtakDTO.fattetDato)
     }
 }
 
