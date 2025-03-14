d1c18340192a48a7518c4f95d53806a9041614d5
Ingrid Fosså <ingrid.fossa@nav.no>
2025-03-14 08:39:36 +0100

    Legg til test for henting av oppfølgingsperiode som fangar opp den fiksa buggen

--- src/test/java/no/nav/veilarbvedtaksstotte/repository/SisteOppfolgingPeriodeRepositoryTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/repository/SisteOppfolgingPeriodeRepositoryTest.kt
@@ -1,7 +1,11 @@
 package no.nav.veilarbvedtaksstotte.repository
 
+import no.nav.common.types.identer.AktorId
 import no.nav.veilarbvedtaksstotte.domain.kafka.KafkaSisteOppfolgingsperiode
+import no.nav.veilarbvedtaksstotte.domain.oppfolgingsperiode.SisteOppfolgingsperiode
 import no.nav.veilarbvedtaksstotte.utils.DatabaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -32,5 +36,59 @@ class SisteOppfolgingPeriodeRepositoryTest : DatabaseTest() {
 
         assertDoesNotThrow { sisteOppfolgingPeriodeRepository.upsertSisteOppfolgingPeriode(oppfolgingsperiodeFraKafka) }
 
+    }
+
+    @Test
+    fun `hente inneværende oppfolgingsperiode når bruker er under oppfølging`() {
+        // given
+        val uuid = UUID.fromString("955b2735-e824-4f2a-b703-76658760a4cc")
+        val aktorId = AktorId.of("2228184718032")
+        val startdato = ZonedDateTime.now()
+        val sluttdato = null
+
+        val oppfolgingsperiodeFraKafka = KafkaSisteOppfolgingsperiode(
+            uuid = uuid,
+            aktorId = aktorId.toString(),
+            startDato = startdato,
+            sluttDato = sluttdato,
+        )
+
+        sisteOppfolgingPeriodeRepository.upsertSisteOppfolgingPeriode(oppfolgingsperiodeFraKafka)
+
+        // when
+        val innevarendeOppfolgingsperiode = sisteOppfolgingPeriodeRepository.hentInnevarendeOppfolgingsperiode(aktorId)
+
+        // then
+        val forventetOppfolgingsperiode = SisteOppfolgingsperiode(
+            oppfolgingsperiodeId = uuid,
+            aktorId = aktorId,
+            startdato = startdato,
+            sluttdato = sluttdato
+        )
+
+        assertEquals(forventetOppfolgingsperiode, innevarendeOppfolgingsperiode)
+    }
+
+    @Test
+    fun `ikkje finne oppfølgingsperiode når bruker ikkje er under oppfølging`() {
+        // given
+        val uuid = UUID.fromString("955b2735-e824-4f2a-b703-76658760a4cc")
+        val aktorId = AktorId.of("2228184718032")
+        val startdato = ZonedDateTime.now().minusDays(1)
+        val sluttdato = ZonedDateTime.now()
+
+        val oppfolgingsperiodeFraKafka = KafkaSisteOppfolgingsperiode(
+            uuid = uuid,
+            aktorId = aktorId.toString(),
+            startDato = startdato,
+            sluttDato = sluttdato,
+        )
+        sisteOppfolgingPeriodeRepository.upsertSisteOppfolgingPeriode(oppfolgingsperiodeFraKafka)
+
+        // when
+        val innevarendeOppfolgingsperiode = sisteOppfolgingPeriodeRepository.hentInnevarendeOppfolgingsperiode(aktorId)
+
+        // then
+        assertNull(innevarendeOppfolgingsperiode)
     }
 }
