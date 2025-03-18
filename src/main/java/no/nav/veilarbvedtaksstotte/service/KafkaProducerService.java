5bbd504f7a5b67e2e191af7e062c8cd23f63f22a
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-03-18 10:26:43 +0100

    Gjeldende § 14 a-vedtak topic: Legg til konfigurasjon og metode for produsering

--- src/main/java/no/nav/veilarbvedtaksstotte/service/KafkaProducerService.java
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/KafkaProducerService.java
@@ -2,9 +2,12 @@ package no.nav.veilarbvedtaksstotte.service;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.kafka.producer.feilhandtering.KafkaProducerRecordStorage;
+import no.nav.common.types.identer.AktorId;
 import no.nav.veilarbvedtaksstotte.config.KafkaProperties;
 import no.nav.veilarbvedtaksstotte.domain.kafka.KafkaVedtakSendt;
 import no.nav.veilarbvedtaksstotte.domain.kafka.KafkaVedtakStatusEndring;
+import no.nav.veilarbvedtaksstotte.domain.vedtak.Gjeldende14aVedtak;
+import no.nav.veilarbvedtaksstotte.domain.vedtak.Gjeldende14aVedtakKafkaDTO;
 import no.nav.veilarbvedtaksstotte.domain.vedtak.Siste14aVedtak;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
@@ -52,6 +55,18 @@ public class KafkaProducerService {
                                 kafkaProperties.getSiste14aVedtakTopic(),
                                 siste14aVedtak.getAktorId().get(),
                                 siste14aVedtak));
+
+        producerRecordStorage.store(producerRecord);
+    }
+
+    public void sendGjeldende14aVedtak(AktorId key, Gjeldende14aVedtakKafkaDTO value) {
+        ProducerRecord<byte[], byte[]> producerRecord = serializeJsonRecord(
+                new ProducerRecord<>(
+                        kafkaProperties.getGjeldende14aVedtakTopic(),
+                        key.get(),
+                        value
+                )
+        );
 
         producerRecordStorage.store(producerRecord);
     }
