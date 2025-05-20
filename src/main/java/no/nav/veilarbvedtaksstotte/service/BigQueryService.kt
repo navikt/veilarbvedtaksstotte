210cfd030c0f0bd0b7aae82832c22e5f4b30d154
Mathias S. Müller <mathias.sundt.muller@nav.no>
2025-05-20 10:35:01 +0200

    Les fra databasen, skriv tilbake nye rader og send til BQ
    Co-authored-by: Julie Hill Roa <julie.hill.roa@nav.no>

--- src/main/java/no/nav/veilarbvedtaksstotte/service/BigQueryService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/BigQueryService.kt
@@ -21,39 +21,51 @@ class BigQueryService(
 
     val log: Logger = LoggerFactory.getLogger(BigQueryService::class.java)
 
-    fun TableId.insertRequest(row: Map<String, Any?>): InsertAllRequest {
-        return InsertAllRequest.newBuilder(this).addRow(row).build()
+    fun TableId.insertRequest(rows: List<Map<String, Any?>>): InsertAllRequest {
+        val request = InsertAllRequest.newBuilder(this)
+        rows.forEach { row -> request.addRow(row) }
+        return request.build()
     }
 
     fun logEvent(sakStatistikk: SakStatistikk) {
-        val vedtakStatistikkRow = mapOf(
-            "sekvensnummer" to sakStatistikk.sekvensnummer?.toBigInteger(),
-            "behandling_id" to sakStatistikk.behandlingId?.toInt(),
-            "aktor_id" to sakStatistikk.aktorId?.get(),
-            "oppfolging_periode_uuid" to sakStatistikk.oppfolgingPeriodeUUID.toString(),
-            "relatert_behandling_id" to sakStatistikk.relatertBehandlingId?.toInt(),
-            "relatert_fagsystem" to sakStatistikk.relatertFagsystem?.name,
-            "sak_id" to sakStatistikk.sakId,
-            "mottatt_tid" to sakStatistikk.mottattTid.toString(),
-            "registrert_tid" to sakStatistikk.registrertTid.toString(),
-            "ferdigbehandlet_tid" to sakStatistikk.ferdigbehandletTid?.toString(),
-            "endret_tid" to sakStatistikk.endretTid.toString(),
-            "sak_ytelse" to sakStatistikk.sakYtelse,
-            "behandling_type" to sakStatistikk.behandlingType?.name,
-            "behandling_status" to sakStatistikk.behandlingStatus?.name,
-            "behandling_resultat" to sakStatistikk.behandlingResultat?.name,
-            "behandling_metode" to sakStatistikk.behandlingMetode?.name,
-            "innsatsgruppe" to sakStatistikk.innsatsgruppe?.name,
-            "hovedmal" to sakStatistikk.hovedmal?.name,
-            "opprettet_av" to sakStatistikk.opprettetAv,
-            "saksbehandler" to sakStatistikk.saksbehandler,
-            "ansvarlig_beslutter" to sakStatistikk.ansvarligBeslutter,
-            "ansvarlig_enhet" to sakStatistikk.ansvarligEnhet?.get(),
-            "fagsystem_navn" to sakStatistikk.fagsystemNavn.name,
-            "fagsystem_versjon" to sakStatistikk.fagsystemVersjon,
-        )
-        val vedtaksstatistikkTilBigQuery = vedtakStatistikkRow.filter { it.value != null }
-        val vedtak14aEvent = vedtakStatistikkTable.insertRequest(vedtaksstatistikkTilBigQuery)
+        logEvent(listOf(sakStatistikk))
+    }
+
+    fun logEvent(sakStatistikk: List<SakStatistikk>) {
+        if (sakStatistikk.isEmpty()) {
+            log.warn("Ingen statistikkrader å sende til BigQuery")
+            return
+        }
+
+        val vedtakStatistikkRader = sakStatistikk.map {
+            mapOf(
+                "sekvensnummer" to it.sekvensnummer?.toBigInteger(),
+                "behandling_id" to it.behandlingId?.toInt(),
+                "aktor_id" to it.aktorId?.get(),
+                "oppfolging_periode_uuid" to it.oppfolgingPeriodeUUID.toString(),
+                "relatert_behandling_id" to it.relatertBehandlingId?.toInt(),
+                "relatert_fagsystem" to it.relatertFagsystem?.name,
+                "sak_id" to it.sakId,
+                "mottatt_tid" to it.mottattTid.toString(),
+                "registrert_tid" to it.registrertTid.toString(),
+                "ferdigbehandlet_tid" to it.ferdigbehandletTid?.toString(),
+                "endret_tid" to it.endretTid.toString(),
+                "sak_ytelse" to it.sakYtelse,
+                "behandling_type" to it.behandlingType?.name,
+                "behandling_status" to it.behandlingStatus?.name,
+                "behandling_resultat" to it.behandlingResultat?.name,
+                "behandling_metode" to it.behandlingMetode?.name,
+                "innsatsgruppe" to it.innsatsgruppe?.name,
+                "hovedmal" to it.hovedmal?.name,
+                "opprettet_av" to it.opprettetAv,
+                "saksbehandler" to it.saksbehandler,
+                "ansvarlig_beslutter" to it.ansvarligBeslutter,
+                "ansvarlig_enhet" to it.ansvarligEnhet?.get(),
+                "fagsystem_navn" to it.fagsystemNavn.name,
+                "fagsystem_versjon" to it.fagsystemVersjon,
+            ).filter { rad -> rad.value != null }
+        }
+        val vedtak14aEvent = vedtakStatistikkTable.insertRequest(vedtakStatistikkRader)
         insertWhileToleratingErrors(vedtak14aEvent)
     }
 
--- src/main/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkResendingService.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/service/SakStatistikkResendingService.kt
@@ -7,22 +7,35 @@ import no.nav.veilarbvedtaksstotte.repository.SakStatistikkRepository.Companion.
 import org.springframework.stereotype.Service
 
 @Service
-class SakStatistikkResendingService (private val sakStatistikkRepository: SakStatistikkRepository) {
+class SakStatistikkResendingService(
+    private val sakStatistikkRepository: SakStatistikkRepository,
+    private val bigQueryService: BigQueryService
+) {
     fun resendStatistikk() {
         // steg 0: lage cron-jobb som trigger funksjon på ønsket tidspunkt (kun EN gang)
-        // :checkmark: steg 1: Hent ut alle rader som vi skal endre, eks. alle rader med behandlig_status='AVBRUTT'
-        // :checkmark: steg 2: Vi har en liste av saksstatistikkrader, går gjennom denne og endrer behandling_status til 'AVSLUTTET' på hver rad. OBS! aldri endre behandling_id og endret_tid, disse to i kombinasjon er en nøkkel for hver rad
-        // steg 3: Insert de endrede radene tilbake i databasen
-        // steg 4: Send til BigQuery – bruk listen fra steg 2
+        // ✓ steg 1: Hent ut alle rader som vi skal endre, eks. alle rader med behandlig_status='AVBRUTT'
+        // ✓ steg 2: Vi har en liste av saksstatistikkrader, går gjennom denne og endrer behandling_status til 'AVSLUTTET' på hver rad. OBS! aldri endre behandling_id og endret_tid, disse to i kombinasjon er en nøkkel for hver rad
+        // ✓ steg 3: Insert de endrede radene tilbake i databasen
+        // ✓ steg 4: Send til BigQuery – bruk listen fra steg 2
 
-        val parameters = mapOf<String, Any>("BEHANDLING_STATUS" to BehandlingStatus.AVBRUTT)
+        val parameters = mapOf<String, Any>("BEHANDLING_STATUS" to BehandlingStatus.AVBRUTT.name)
 
         val sql = "SELECT * FROM $SAK_STATISTIKK_TABLE WHERE behandling_status = :BEHANDLING_STATUS"
 
         val sakStatistikkRader = sakStatistikkRepository.hentSakStatistikkListe(sql, parameters)
 
         // Lager de nye radene, pass på å slette/ikke sette sekvensnummer, det settes automatisk ved innsetting i databasen
-        sakStatistikkRader.map { it -> it.copy(sekvensnummer = null, behandlingStatus = BehandlingStatus.AVSLUTTET, behandlingResultat = BehandlingResultat.AVBRUTT ) }
+        val endredeRader = sakStatistikkRader.map { it ->
+            it.copy(
+                sekvensnummer = null,
+                behandlingStatus = BehandlingStatus.AVSLUTTET,
+                behandlingResultat = BehandlingResultat.AVBRUTT
+            )
+        }
+
+        val lagredeDatabaseRader = sakStatistikkRepository.insertSakStatistikkRadBatch(endredeRader)
+
+        bigQueryService.logEvent(lagredeDatabaseRader)
     }
 
 }
