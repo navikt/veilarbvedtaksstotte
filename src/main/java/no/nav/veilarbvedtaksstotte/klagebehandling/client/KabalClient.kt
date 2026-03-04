b99bff348a24cb2e59982a287a2753f0afac5c00
Kristina Skatun <kristinaskatun@gmail.com>
2026-03-04 14:17:12 +0100

    Mer logging

--- src/main/java/no/nav/veilarbvedtaksstotte/klagebehandling/client/KabalClient.kt
+++ src/main/java/no/nav/veilarbvedtaksstotte/klagebehandling/client/KabalClient.kt
@@ -6,6 +6,8 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.slf4j.LoggerFactory
 import java.util.function.Supplier
+import kotlin.code
+import kotlin.text.get
 
 
 interface KabalClient {
@@ -19,7 +21,33 @@ class KabalClientImpl(
     private val client: OkHttpClient = RestClient.baseClient()
     private val log = LoggerFactory.getLogger(KabalClientImpl::class.java)
 
+//    override fun sendKlageTilKabal(klageDto: KabalDTO) {
+//        val request = Request.Builder()
+//            .url("$url/api/oversendelse/v4/sak")
+//            .header("Authorization", "Bearer ${machineToMachineTokenClient.get()}")
+//            .post(RestUtils.toJsonRequestBody(klageDto))
+//            .build()
+//
+//        client.newCall(request).execute().use { response ->
+//            if (!response.isSuccessful) {
+//                val message =
+//                    "Uventet status ${response.code} ved kall mot Kabal for klageId ${klageDto.kildeReferanse} med melding ${response.message}"
+//                log.error(message)
+//                throw RuntimeException(message)
+//            }
+//        }
+//    }
+
     override fun sendKlageTilKabal(klageDto: KabalDTO) {
+        val jsonBody = RestUtils.toJsonRequestBody(klageDto)
+        log.debug("Sending to Kabal: {} - Body: {}", jsonBody.contentType(), klageDto)
+
+        // Or to get the actual JSON string:
+        val objectMapper = no.nav.common.json.JsonUtils.getMapper()
+        val jsonString = objectMapper.writeValueAsString(klageDto)
+        log.info("Request body: $jsonString")
+        println("Request body: $jsonString")
+
         val request = Request.Builder()
             .url("$url/api/oversendelse/v4/sak")
             .header("Authorization", "Bearer ${machineToMachineTokenClient.get()}")
@@ -29,12 +57,14 @@ class KabalClientImpl(
         client.newCall(request).execute().use { response ->
             if (!response.isSuccessful) {
                 val message =
-                    "Uventet status ${response.code} ved kall mot Kabal for klageId ${klageDto.kildeReferanse} med melding ${response.message}"
+                    "Uventet status ${response.code} ved kall mot Kabal for klageId ${klageDto.kildeReferanse} " +
+                            "med melding ${response.message} og json $jsonBody"
                 log.error(message)
                 throw RuntimeException(message)
             }
         }
     }
+
 
     // TODO - legg til healthcheck?
 
