1db1d0d7dbe4388dd079f3f918bcf1813e7e5e25
Kristina Skatun <kristinaskatun@gmail.com>
2026-06-29 12:56:39 +0200

    Fiks bønne i adming controller test

--- src/test/java/no/nav/veilarbvedtaksstotte/controller/AdminControllerTest.kt
+++ src/test/java/no/nav/veilarbvedtaksstotte/controller/AdminControllerTest.kt
@@ -1,6 +1,7 @@
 package no.nav.veilarbvedtaksstotte.controller
 
 import no.nav.veilarbvedtaksstotte.controller.AdminController.POAO_ADMIN
+import no.nav.veilarbvedtaksstotte.repository.VedtaksstotteRepository
 import no.nav.veilarbvedtaksstotte.service.AuthService
 import no.nav.veilarbvedtaksstotte.service.KafkaRepubliseringService
 import no.nav.veilarbvedtaksstotte.service.VedtakService
@@ -9,10 +10,10 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
-import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 
 @WebMvcTest(AdminController::class)
 class AdminControllerTest {
@@ -24,7 +25,10 @@ class AdminControllerTest {
     lateinit var kafkaRepubliseringService: KafkaRepubliseringService
 
     @MockitoBean
-    lateinit var vedtakService : VedtakService
+    lateinit var vedtakService: VedtakService
+
+    @MockitoBean
+    lateinit var vedtaksstotteRepository: VedtaksstotteRepository
 
     @Autowired
     lateinit var mockMvc: MockMvc
